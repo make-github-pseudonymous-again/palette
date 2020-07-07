f91f18ef2a1ba5142718898a0264f5208eeb9032
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-07-07 16:53:46 +0200

    :sparkles: feat: Use CssBaseline.

--- client/main.js
+++ client/main.js
@@ -6,6 +6,8 @@ import { render } from 'react-dom';
 import { DndProvider } from 'react-dnd' ;
 import { HTML5Backend } from 'react-dnd-html5-backend' ;
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import App from '../imports/ui/App.js';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
@@ -16,6 +18,7 @@ render(
 	<DndProvider backend={HTML5Backend}>
 		<SnackbarProvider maxSnack={3}>
 			<Router>
+				<CssBaseline/>
 				<App/>
 			</Router>
 		</SnackbarProvider>
