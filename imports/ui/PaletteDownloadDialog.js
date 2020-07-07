0a57d5c25c9c12bc1a6808e523e81ee8d4d8a7e3
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-07-07 19:45:09 +0200

    :art: style(PaletteDownloadDialog): Remove unnecessary import.

--- imports/ui/PaletteDownloadDialog.js
+++ imports/ui/PaletteDownloadDialog.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from './modal/OptimizedDialog.js';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
